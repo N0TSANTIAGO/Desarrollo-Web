a298ac25faad52b18107616a3648e4c0e0d9f7ab
Santiago Echavarria <42123750+N0TSANTIAGO@users.noreply.github.com>
2025-04-08 20:42:37 -0500

    Error Solution
    Solucionando Problema menor

--- TIA 3/proyectos_pia/server/config/database.js
+++ TIA 3/proyectos_pia/server/config/database.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'piscinas_db',
-  password: 'tu_contraseña',
+  database: 'proyectos_pia',
+  password: 'postgres',
   port: 5432,
 });
 
