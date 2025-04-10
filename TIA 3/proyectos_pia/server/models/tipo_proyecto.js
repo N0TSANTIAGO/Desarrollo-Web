b151c0a25fc2c83901d3926064c3da150ff1e1bc
Santiago E <42123750+N0TSANTIAGO@users.noreply.github.com>
2025-04-09 20:29:49 -0500

    Update and rename Usuario.js to tipo_proyecto.js
    Cambios de configuración del servidor parte 1

---
+++ TIA 3/proyectos_pia/server/models/tipo_proyecto.js
@@ -0,0 +1,11 @@
+class tipo_proyecto {
+    constructor(id, codigo, descripcion, abreviatura, fecha_registro) {
+      this.id = id;
+      this.codigo = codigo;
+      this.descripcion = descripcion;
+      this.abreviatura = abreviatura;
+      this.fecha_registro = fecha_registro;
+    }
+  }
+  
+  module.exports = tipo_proyecto;
